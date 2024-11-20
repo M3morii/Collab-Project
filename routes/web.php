41ef695c7f8ba89a6c2f96e6bea803c5304e0dec
Alifakihfahrian <fakihfakhriansyah@gmail.com>
2024-11-20 21:40:17 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,7 @@ use App\Http\Controllers\Web\ClassController;
 
     Route::post('login', [AuthController::class, 'login']);
     Route::post('register', [AuthController::class, 'register']);
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
 
 
 // Admin routes
